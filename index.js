b415c67870f0736601a93642e2674971e1bec3ed
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-06-17 15:26:45 -0400

    Update colors to match latest from Pxls

--- index.js
+++ index.js
@@ -62,106 +62,105 @@ window.addEventListener("load", () => {
 });
 
 const pxls = [{
-    color: "#FFFFFF",
-    name: "White",
-  },
-  {
-    color: "#CDCDCD",
-    name: "Silver",
-  },
-  {
-    color: "#888888",
-    name: "Light Gray",
-  },
-  {
-    color: "#555555",
-    name: "Medium Gray",
-  },
-  {
-    color: "#222222",
-    name: "Dark Gray",
-  },
-  {
-    color: "#000000",
-    name: "Black",
-  },
-  {
-    color: "#FFA7D1",
-    name: "Pink",
-  },
-  {
-    color: "#E50000",
-    name: "Red",
-  },
-  {
-    color: "#800000",
-    name: "Dark Red",
-  },
-  {
-    color: "#FFDDCA",
-    name: "Cream",
-  },
-  {
-    color: "#F6B389",
-    name: "Tan",
-  },
-  {
-    color: "#E59500",
-    name: "Orange",
-  },
-  {
-    color: "#A06A42",
-    name: "Brown",
-  },
-  {
-    color: "#604028",
-    name: "Dark Brown",
-  },
-  {
-    color: "#E5D900",
-    name: "Yellow",
-  },
-  {
-    color: "#94E044",
-    name: "Lime",
-  },
-  {
-    color: "#02BE01",
-    name: "Green",
-  },
-  {
-    color: "#005F00",
-    name: "Dark Green",
-  },
-  {
-    color: "#00D3DD",
-    name: "Teal",
-  },
-  {
-    color: "#0083C7",
-    name: "Cyan",
-  },
-  {
-    color: "#0000EA",
-    name: "Blue",
-  },
-  {
-    color: "#CF6EE4",
-    name: "Lavender",
-  },
-  {
-    color: "#FF00FF",
-    name: "Hot Pink",
-  },
-  {
-    color: "#66033C",
-    name: "Purple",
-  },
-  {
-    color: "transparent",
-    name: "*Transparent",
-  },
-];
+	color: "#FFFFFF",
+	name: "White",
+},
+{
+	color: "#CDCDCD",
+	name: "Silver",
+},
+{
+	color: "#888888",
+	name: "Light Gray",
+},
+{
+	color: "#555555",
+	name: "Medium Gray",
+},
+{
+	color: "#222222",
+	name: "Dark Gray",
+},
+{
+	color: "#000000",
+	name: "Black",
+},
+{
+	color: "#FFA7D1",
+	name: "Pink",
+},
+{
+	color: "#E50000",
+	name: "Red",
+},
+{
+	color: "#800000",
+	name: "Dark Red",
+},
+{
+	color: "#FFDDCA",
+	name: "Cream",
+},
+{
+	color: "#F6B389",
+	name: "Tan",
+},
+{
+	color: "#E59500",
+	name: "Orange",
+},
+{
+	color: "#A06A42",
+	name: "Brown",
+},
+{
+	color: "#604028",
+	name: "Dark Brown",
+},
+{
+	color: "#E5D900",
+	name: "Yellow",
+},
+{
+	color: "#94E044",
+	name: "Lime",
+},
+{
+	color: "#02BE01",
+	name: "Green",
+},
+{
+	color: "#005F00",
+	name: "Dark Green",
+},
+{
+	color: "#00D3DD",
+	name: "Teal",
+},
+{
+	color: "#0083C7",
+	name: "Cyan",
+},
+{
+	color: "#0000EA",
+	name: "Blue",
+},
+{
+	color: "#CF6EE4",
+	name: "Lavender",
+},
+{
+	color: "#FF00FF",
+	name: "Hot Pink",
+},
+{
+	color: "#820080",
+	name: "Purple",
+},
+{
+	color: "transparent",
+	name: "*Transparent",
+}];
 
 Array.from(document.querySelectorAll("select, input:not(.noConfig)")).forEach(element => {
   if (element.nodeName === "SELECT") {
