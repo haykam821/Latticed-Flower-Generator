69e5a48930e774bbedde4421b13c314e3f5e4358
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-01-27 13:34:36 -0500

    Reimplement color randomization

--- src/components/cards/colors-card.jsx
+++ src/components/cards/colors-card.jsx
@@ -4,7 +4,23 @@ const styled = require("styled-components").default;
 const { Card, Button } = require("react-bootstrap");
 const ColorSelector = require("../color-selector.jsx");
 
+const colors = require("../../util/colors.js");
+const randomItem = require("random-item");
+
 class ColorsCardUnstyled extends React.Component {
+	constructor(props) {
+		super(props);
+		this.randomizeColors = this.randomizeColors.bind(this);
+	}
+
+	randomizeColors() {
+		const configKeys = Object.keys(this.props.config.store);
+		for (const key of configKeys) {
+			if (!key.endsWith("Color")) continue;
+			this.props.config.set(key, randomItem(colors).color);
+		}
+	}
+
 	render() {
 		return <Card className="bg-dark">
 			<Card.Body>
@@ -31,7 +47,7 @@ class ColorsCardUnstyled extends React.Component {
 				<ColorSelector title="Alternate Dirt" id="altDirtColor" config={this.props.config} />
 				<br />
 				<br />
-				<Button variant="secondary" block id="rand" title="Feeling lucky? Click this button to randomize the colors!">
+				<Button variant="secondary" block title="Feeling lucky? Click this button to randomize the colors!" onClick={this.randomizeColors}>
 					Randomize Colors
 				</Button>
 			</Card.Body>
