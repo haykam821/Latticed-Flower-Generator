2db4fad285f6e6253023ce293c460e1e6df47e4d
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-01-27 00:26:11 -0500

    Use a Webpack plugin to generate the PWA manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const ManifestPlugin = require("webpack-pwa-manifest");
+
 module.exports = {
 	entry: "./src/index.js",
 	mode: process.env.WEBPACK_MODE || "production",
@@ -7,4 +9,18 @@ module.exports = {
 		filename: "index.js",
 		path: path.resolve(__dirname, "./dist"),
 	},
+	plugins: [
+		new ManifestPlugin({
+			/* eslint-disable camelcase */
+			background_color: "#222222",
+			display: "standalone",
+			fingerprints: false,
+			inject: false,
+			lang: "en",
+			name: "Latticed Flower Generator",
+			short_name: "Flower Gen",
+			theme_color: "#6c757d",
+			/* eslint-enable camelcase */
+		}),
+	],
 };
